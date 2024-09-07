73c4efb7959e4ccfb3c36aeccdd793e8071f6d2a
Chilzbrochill <109883039+Chilzbrochill@users.noreply.github.com>
2024-09-07 13:01:26 +0700

    cmt

--- TestSudoku/app/src/main/java/com/example/testsudoku/SudokuMenu.java
+++ TestSudoku/app/src/main/java/com/example/testsudoku/SudokuMenu.java
@@ -3,7 +3,7 @@ package com.example.testsudoku;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,23 +11,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Menu extends AppCompatActivity {
+public class SudokuMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_sudoku_menu);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        ImageView sudoku = findViewById(R.id.img_Sudoku);
-        sudoku.setOnClickListener(new View.OnClickListener() {
+        Button newGame = findViewById(R.id.button_newgameSudoku);
+        newGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Menu.this, SudokuMenu.class);
+                Intent i = new Intent(SudokuMenu.this, Sudoku.class);
                 startActivity(i);
             }
         });
